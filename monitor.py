d8462f800b6d5826f7707c14cb06a5511896db99
Simone Trubian <simone.trubian@gmail.com>
2015-11-18 22:36:53 +0000

    Linted file.

--- monitor.py
+++ monitor.py
@@ -1,18 +1,14 @@
-import string
 import urllib.request
-
-import requests
-from lxml import html
 import shutil
-import tempfile
 
-f1 = open('file1','r')
-f2 = open('file2','wb')
+
+f1 = open('file1', 'r')
+f2 = open('file2', 'wb')
 first = '<title>'
+
 for line in f1:
- print (line)
- with urllib.request.urlopen(line) as response:  
-   shutil.copyfileobj(response, f2)
-   if first in open('file2').read():
-    print ('trovato')
-   
+    print(line)
+    with urllib.request.urlopen(line) as response:
+        shutil.copyfileobj(response, f2)
+        if first in open('file2').read():
+            print('trovato')
