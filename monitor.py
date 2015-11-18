31eee69ad627d2d760100951a7c6f268c2cc76e7
Simone Trubian <simone.trubian@gmail.com>
2015-11-18 23:02:35 +0000

    Moved url file read to a function.

--- monitor.py
+++ monitor.py
@@ -2,13 +2,20 @@ import urllib.request
 import shutil
 
 
-f1 = open('file1', 'r')
-f2 = open('file2', 'wb')
-first = '<title>'
+def get_urls():
+    url_list = []
+    with open('file1', 'r') as f1:
+        for line in f1:
+            url_list.append(line)
 
-for line in f1:
-    print(line)
-    with urllib.request.urlopen(line) as response:
-        shutil.copyfileobj(response, f2)
-        if first in open('file2').read():
-            print('trovato')
+    return url_list
+
+#f2 = open('file2', 'wb')
+#first = '<title>'
+#
+#for line in f1:
+#    print(line)
+#    with urllib.request.urlopen(line) as response:
+#        shutil.copyfileobj(response, f2)
+#        if first in open('file2').read():
+#            print('trovato')
